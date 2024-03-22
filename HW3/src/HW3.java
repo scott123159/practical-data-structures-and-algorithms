c9cc1fcf80af243f0b243a730d8646be1163a197
You-Hua Liao <r11631041@ntu.edu.tw>
2024-03-22 08:28:47 +0800

    HW3 score 100/100

--- HW3/src/HW3.java
+++ HW3/src/HW3.java
@@ -1,10 +1,10 @@
 import java.util.Arrays; // Used to print the arrays
+import java.util.Stack;
 
 class member{
     int Level;
     int Range;
     int Index;
-
     member(int _level,int _range, int i){
         Level=_level;
         Range=_range;
@@ -16,38 +16,41 @@ class Mafia {
     public int[] result(int[] levels, int[] ranges) {
         int n = levels.length;
         member[] members = new member[n];
-        int[] result = new int[n * 2];
-
         for (int i = 0; i < n; i++) {
             members[i] = new member(levels[i], ranges[i], i);
         }
+        int[] answer = new int[n * 2];
 
+        int[] a = new int[n];
+        Stack<member> stack = new Stack<>();
         for (int i = 0; i < n; i++) {
-            int currIndex = members[i].Index;
-            int minIndex = currIndex, maxIndex = currIndex;
-            int leftIndex = currIndex - 1;
-            int rightIndex = currIndex + 1;
-            while (leftIndex >= 0 && Math.abs(currIndex - leftIndex) <= members[i].Range) {
-                if (members[leftIndex].Level >= members[currIndex].Level) break;
-                minIndex = leftIndex;
-                leftIndex--;
+            while (!stack.isEmpty() && stack.peek().Level < members[i].Level) {
+                stack.pop();
             }
-            while (rightIndex < n && Math.abs(currIndex - rightIndex) <= members[i].Range) {
-                if (members[rightIndex].Level >= members[currIndex].Level) break;
-                maxIndex = rightIndex;
-                rightIndex++;
-            }
-            result[2 * i] = minIndex;
-            result[2 * i + 1] = maxIndex;
+            a[i] = stack.isEmpty() ? -1 : stack.peek().Index;
+            stack.push(members[i]);
         }
-        // Given the traits of each member and output
-        // the leftmost and rightmost index of member
-        // can be attacked by each member.
-        return result;
-        // complete the code by returning an int[]
-        // flatten the results since we only need an 1-dimentional array.
+        stack.clear();
+        int[] b = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek().Level < members[i].Level) {
+                stack.pop();
+            }
+            b[i] = stack.isEmpty() ? -1 : stack.peek().Index;
+            stack.push(members[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            int leftIndex, rightIndex, rangeI = members[i].Range;
+            leftIndex = Math.max(Math.max(0, i - rangeI), (a[i] != -1) ? a[i] + 1 : 0);
+            rightIndex = Math.min(n - 1, Math.min(i + rangeI, (b[i] != -1) ? b[i] - 1 : n - 1));
+            answer[i * 2] = leftIndex;
+            answer[i * 2 + 1] = rightIndex;
+        }
+        return answer;
     }
+}
 
+class HW3 {
     public static void main(String[] args) {
         Mafia sol = new Mafia();
         System.out.println(Arrays.toString(
@@ -55,10 +58,5 @@ class Mafia {
                         new int[] { 1,  8,  1, 7,  2})));
         // Output: [0, 0, 0, 3, 2, 3, 3, 3, 2, 4]
         //      => [a0, b0, a1, b1, a2, b2, a3, b3, a4, b4]
-    }
-}
-
-class HW3 {
-    public static void main(String[] args) {
     }
 }
